3e7d6ecd1abd90f66d540f7af139eec529dd33d7
fatihay53 <f.sultanay@gmail.com>
2021-04-02 20:09:12 -0400

    app

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -11,19 +11,19 @@ function Navbar() {
                 <div className="container">
                     <ul className="nav navbar-expand-lg navbar-light top_nav">
                         <li><a href="#"><i className="fa fa-phone" />+1 (613) 838 5042</a></li>
-                        <li><a href="#"><i className="fa fa-envelope" />info@goldiemohrltd.ca</a></li>
+                        <li><a href="contact"><i className="fa fa-envelope" />info@goldiemohrltd.ca</a></li>
                         <li><a href="#"><i className="fa fa-clock" />Mon - Sat 12:00 - 20:00</a></li>
                         <li><a target="_blank" href="https://www.facebook.com/GoldieMohr"><i class="fab fa-facebook-f"></i></a></li>
                         <li><a target="_blank" href="https://twitter.com/goldiemohrltd"><i class="fab fa-twitter"></i></a></li>
                         <li className="ms-auto" id="faUser"><a href="#"></a></li>
-                        <div class="dropdown">
+                        {/* <div class="dropdown">
                             <button class="dropbtn"><i class="fas fa-user fa-lg" ></i></button>
                             <div class="dropdown-content">
                                 <a href="./Training">Training</a>
                                 <a href="#">Profile</a>
                                 <a href="#">Logout</a>
                             </div>
-                        </div>
+                        </div> */}
                     </ul>
                 </div>
             </section>
