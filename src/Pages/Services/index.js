1ab7eb69217faf34de0d52bd7d125c828d5ce91a
Fatih Sultan Ay <f.sultanay@gmail.com>
2021-04-05 03:26:46 -0400

    Update index.js

--- src/Pages/Services/index.js
+++ src/Pages/Services/index.js
@@ -414,7 +414,7 @@ function Services() {
 
         <div className="tittle wow fadeInUp">
           <h2>Our Featured Works</h2>
-          <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4>
+          <h4>Please check 2021 Projects below</h4>
         </div>
         <div className="featured_gallery">
 
